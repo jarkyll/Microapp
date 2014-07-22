667887678539078f61702cafd35935a20fb34d58
Shane Bammel <bammels@dnb.com>
2014-07-22 22:07:57 +0000

    Comment out deprecated DEBUG_TOOLBAR_CONFIG.

--- project_name/settings/dev.py
+++ project_name/settings/dev.py
@@ -69,7 +69,7 @@ MIDDLEWARE_CLASSES += (
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False,
-}
+#DEBUG_TOOLBAR_CONFIG = {
+#    'INTERCEPT_REDIRECTS': False,
+#}
 ########## END TOOLBAR CONFIGURATION
